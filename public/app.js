41d881f715cc6b4f70025c64d0b0195360901a40
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-10 23:03:33 +0100

    Add infoWindow within addMarker()

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
 var initialize = function(){
   var center = {lat: 23.145140, lng: -82.381415};
   var container = document.getElementById('main-map');
-  var mainMap = new MapWrapper(container, center, 15);
+
+  var contentString = '<div id="content">The Malecon (officially Avenida de Maceo) is a broad esplanade, roadway and seawall which stretches for 8 km (5 miles) along the coast in Havana, Cuba, from the mouth of Havana Harbor in Old Havana, along the north side of the Centro Habana neighbourhood, ending in the Vedado neighbourhood. New businesses are appearing on the esplanade due to economic reforms in Cuba that now allow Cubans to own private businesses.</div>';
+
+  var mainMap = new MapWrapper(container, center, 15, contentString);
 
   const bounceButton = document.querySelector('#button-bounce-markers');
   bounceButton.addEventListener('click', mainMap.bounceMarkers.bind(mainMap));
 
+  // mainMap.addInfoWindow();
   mainMap.addMarker(center);
   mainMap.addClickEvent();
 };
--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -1,18 +1,41 @@
-var MapWrapper = function(container, coords, zoom){
+var MapWrapper = function(container, coords, zoom, infoWindowText){
   this.googleMap = new google.maps.Map(container, {
     center: coords,
     zoom: zoom
   });
+  this.infoWindowText = infoWindowText;
   this.markers = [];
 }
 
+// -----Try to add info window--------
+
 MapWrapper.prototype.addMarker = function (coords) {
+  var infowindow = new google.maps.InfoWindow({
+    content: this.infoWindowText
+  });
+
   var marker = new google.maps.Marker({
     position: coords,
     map: this.googleMap
   });
   this.markers.push(marker);
+
+  marker.addListener('click', function() {
+    infowindow.open(this.googleMap, marker);
+  });
+
 };
+
+// --------------
+
+
+// MapWrapper.prototype.addMarker = function (coords) {
+//   var marker = new google.maps.Marker({
+//     position: coords,
+//     map: this.googleMap
+//   });
+//   this.markers.push(marker);
+// };
 
 MapWrapper.prototype.addClickEvent = function(){
   google.maps.event.addListener(this.googleMap, 'click', function(event){
